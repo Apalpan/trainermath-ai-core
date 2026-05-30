b11f037b6eca0b21f4daa569ee7c75779ef441fd
Alejandro Palpan <apalpan@genplusdesign.com>
2026-05-29 20:02:12 -0500

    fix coneic web safe area provider

--- coneic-cusco-2026-mobile/src/App.tsx
+++ coneic-cusco-2026-mobile/src/App.tsx
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import { AppNavigator } from './navigation/AppNavigator';
 
 export default function ConeicApp() {
   return (
-    <>
+    <SafeAreaProvider>
       <StatusBar style="light" />
       <AppNavigator />
-    </>
+    </SafeAreaProvider>
   );
 }
