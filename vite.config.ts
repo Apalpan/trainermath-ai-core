56e290c7fe68b3c9c707988cff467f27b928cf4b
Alejandro Palpan <apalpan@genplusdesign.com>
2026-06-06 22:43:52 -0500

    Add TrainerMath asset cache busting

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: './',
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name]-mathfocus-[hash].js',
+        chunkFileNames: 'assets/[name]-mathfocus-[hash].js',
+        assetFileNames: 'assets/[name]-mathfocus-[hash][extname]',
+      },
+    },
+  },
 });
